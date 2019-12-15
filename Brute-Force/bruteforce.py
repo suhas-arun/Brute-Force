82c1aecf5dfa992fa1e44705ac52f100ab833c1e
suhas-arun <suhas.arun142@gmail.com>
2019-12-15 19:02:06 +0000

    Formatting changes

--- Brute-Force/bruteforce.py
+++ Brute-Force/bruteforce.py
@@ -7,23 +7,23 @@ def extract_file(zip_file, password):
         zip_file.extractall(pwd=password)
         return True
     except:
-        pass 
+        pass
 
 
 # creates a list storing all the possible passwords
 words: list = []
-with open("words.txt", 'r') as f:
+with open("words.txt", "r") as f:
     for word in f:
         words.append(word.rstrip("\n"))
 
-ZIP_FILE = zipfile.ZipFile('test.zip')
+ZIP_FILE = zipfile.ZipFile("test.zip")
 
 for word in words:
-    print("Trying \"{}\"".format(word))
+    print('Trying "{}"'.format(word))
     # If the file was extracted, you found the right password.
     if extract_file(ZIP_FILE, str.encode(word)):
-        print('\n\nSuccess!')
-        print('Password found: {}'.format(word))
+        print("\n\nSuccess!")
+        print("Password found: {}".format(word))
         exit(0)
 
 print("Password not found.")
